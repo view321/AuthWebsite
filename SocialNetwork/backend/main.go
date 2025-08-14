e86b94c95a73c628ee49086a2c9187045af0e897
view321 <57069886+view321@users.noreply.github.com>
2025-08-14 18:01:45 +0300

    Update main.go

--- SocialNetwork/backend/main.go
+++ SocialNetwork/backend/main.go
@@ -1,41 +1,48 @@
 package main
 
 import (
-	"log"
 	"database/sql"
+	"log"
+	"strconv"
 
-	"github.com/gofiber/fiber/v2"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
 )
+
+type NoteData struct{
+	Text      string  `json:"text"`
+	Longitude float64 `json:"longitude"`
+	Lattitude float64 `json:"lattitude"`
+}
 
 var notes_db *sql.DB
 
 func main() {
 	dsn := "root:347347@tcp(127.0.0.1:3306)/notes_db"
-	db, err := sql.Open("mysql", dsn)
+	var err error
+	notes_db, err = sql.Open("mysql", dsn)
+	defer notes_db.Close()
 	if err != nil{
 		log.Fatal(err)
 	}
-	if err = db.Ping(); err != nil{
+	if err = notes_db.Ping(); err != nil{
 		log.Fatal(err)
 	}
 	app := fiber.New()
 	api := app.Group("/api")
 	api.Post("/create_note", CreateGeoNote)
 	api.Get("/get_by_id/:id", GetNoteByID)
+	api.Delete("/delete_by_id/:id", DeleteNoteByID)
+	api.Patch("/update_by_id/:id", UpdateNoteByID)
 	log.Fatal(app.Listen(":8080"))
 }
 
 func CreateGeoNote(c *fiber.Ctx) error {
-	var data struct {
-		Text      string  `json:"text"`
-		Longitude float64 `json:"longitude"`
-		Lattitude float64 `json:"lattitude"`
-	}
+	var data NoteData
 	if err := c.BodyParser(&data); err != nil {
 		return c.Status(400).JSON(fiber.Map{"error":"Invalid request"})
 	}
-	_, err := notes_db.Exec("INSERT INTO notes (text, longitude, lattitude) VALUES (?, ?, ?)", data.Text, data.Longitude, data.Lattitude)
+	_, err := notes_db.Exec("INSERT INTO notes (note_text, longitude, lattitude) VALUES (?, ?, ?)", data.Text, data.Longitude, data.Lattitude)
 	if err != nil{
 		return c.Status(500).JSON(fiber.Map{"error" : "DB error"})
 	}
@@ -43,6 +50,49 @@ func CreateGeoNote(c *fiber.Ctx) error {
 }
 
 func GetNoteByID(c *fiber.Ctx) error{
-	id := c.Params("id")
-	
+	id_str := c.Params("id")
+	id, err := strconv.Atoi(id_str)
+	if err != nil{
+		return c.Status(400).JSON(fiber.Map{"error":"Invalid request"})
+	}
+	var data NoteData
+	row := notes_db.QueryRow("SELECT note_text, longitude, lattitude FROM notes WHERE id = ?", id)
+	err = row.Scan(&data.Text, &data.Longitude, &data.Lattitude)
+	if err != nil{
+		return c.Status(400).JSON(fiber.Map{"error":"Invlaid database request"})
+	}
+	return c.JSON(data)
+}
+
+func DeleteNoteByID(c *fiber.Ctx) error{
+	id_str := c.Params("id")
+	id, err := strconv.Atoi(id_str)
+	if err != nil{
+		return c.Status(400).JSON(fiber.Map{"error":"Invalid request"})
+	}
+	_, err = notes_db.Exec("DELETE FROM notes WHERE id = ?", id)
+	if err != nil{
+		return c.Status(400).JSON(fiber.Map{"error":"Invlaid database request"})
+	}
+	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{"message":"Successfully deleted note"})
+}
+
+func UpdateNoteByID(c *fiber.Ctx) error{
+	var data struct{
+		Id int `json:"id"`
+		Text      string  `json:"text"`
+		Longitude float64 `json:"longitude"`
+		Lattitude float64 `json:"lattitude"`
+	}
+	err := c.BodyParser(&data)
+	if err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"wrong json"})
+	}
+	res, err := notes_db.Exec("UPDATE notes SET note_text=? longitude=? lattitude=? WHERE id=?", data.Text, data.Longitude, data.Lattitude, data.Id)
+	if err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"database error"})
+	}
+	c.Status(fiber.StatusAccepted)
+	c.JSON(res)
+	return nil
 }
